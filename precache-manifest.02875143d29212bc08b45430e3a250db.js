23b4552316309d4a8dbda41ab9cac366bbba1362
hy <chessg55@gmail.com>
2020-07-04 15:49:50 +0900

    Updates

---
+++ precache-manifest.02875143d29212bc08b45430e3a250db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06ee8473f1bdcf8a2bd412921412f10",
+    "url": "/movie2020/index.html"
+  },
+  {
+    "revision": "9a5f519adad6563a87d8",
+    "url": "/movie2020/static/css/main.b303c8c9.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5f519adad6563a87d8",
+    "url": "/movie2020/static/js/main.2e9c373f.chunk.js"
+  },
+  {
+    "revision": "60b49d805227aac8dacb",
+    "url": "/movie2020/static/js/runtime-main.bbdf2a7e.js"
+  }
+]);
