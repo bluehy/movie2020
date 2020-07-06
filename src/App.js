8c22fc4d03ac972be011021e322f60b230e62cc5
hy <chessg55@gmail.com>
2020-07-06 16:22:49 +0900

    fix router error and fixed css

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ import About from "./routes/About";
 
 function App(){
   return <HashRouter>
-    <Route path="/home">
-      <h1>Home</h1>
-    </Route>
-    <Route path="/home/introduction">
-      <h1>Introduction</h1>
-    </Route>
-    <Route path="/about">
-      <h1>About</h1>
-    </Route>
+    <Route path="/" exact={true} component={Home}/>
+    <Route path="/about" component={About}/>
     {/* Route는 두개의 props를 가지게 되는데, 하나는 렌더링할 스크린/ 하나는 무슨 명령을 수행할지.
     여기서는 /about에 접속할 경우, About 컴포넌트를 불러오는 기능을 지정한 것. */}
   </HashRouter>
