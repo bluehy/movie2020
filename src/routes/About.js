619e552ce067c022d1083c67fedb0a1cce77958a
hy <chessg55@gmail.com>
2020-07-06 16:54:26 +0900

    fixed about css

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./About.css";
 
-function About(){
-   return <span>About this page: I built it because I love movies.</span>
+function About(props){
+   console.log(props)
+   return (
+      <div className="about__container">
+         <span>
+            "빛은 위와 아래에서 글자들의 내부 공간과 그들 사이의 공간으로 흘러들어 간다."
+         </span>
+         <span>요스트 호훌리(김형진 옮김), 『마이크로 타이포그래피』</span>
+      </div>
+   )
 }
 
 export default About;
